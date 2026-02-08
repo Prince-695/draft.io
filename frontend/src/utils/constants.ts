6ad43b82209119490af74742916135aff9fe4345
Prince Rathod <rathodprince411@gmail.com>
2026-02-08 22:03:36 +0530

    feat: add questionnaire route to constants

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -9,6 +9,7 @@ export const ROUTES = {
   HOME: '/',
   SIGN_IN: '/sign-in',
   SIGN_UP: '/sign-up',
+  QUESTIONNAIRE: '/questionnaire',
   DASHBOARD: '/dashboard',
   PROFILE: '/profile',
   EDIT_PROFILE: '/profile/edit',
